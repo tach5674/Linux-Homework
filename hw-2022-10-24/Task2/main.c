3a0e608593f63c5d6d6edff01e4f6102d505126e
Mikayel <tach5674@gmail.com>
2022-10-25 18:44:32 +0400

    task2

--- hw-2022-10-24/Task2/main.c
+++ hw-2022-10-24/Task2/main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
-	int buff;
-	int i = 0;
-	int fd = open(argv[2], O_RDWR | O_CREAT, 0600); 
-	if(argv[1] == "save"){ 
-		while(scanf("%d", &buff) != -1){
-			
-			++i;
+	if(argc < 3) return 1;
+	char prebuff[32];
+	int buff; 
+	int a;
+	if(!strcmp(argv[1], "save")){ 
+		int fd; 
+		if((fd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0600)) == -1){
+			perror(argv[2]);
+			return 1;
 		}
+		while(a = scanf("%d", &buff) == 1){
+			//sscanf(prebuff, "%d", &buff);
+			write(fd, &buff, sizeof(int));
+			if(getchar() == '\n') break;
+		}
+		if(a != 1) return 1;
+		close(fd);
+		return 0;
 	}
+	if(!strcmp(argv[1], "print")){
+		int fd;
+		if((fd = open(argv[2], O_RDONLY)) == -1){
+			perror(argv[2]);
+			return 1;
+		}
+		while(read(fd, &buff, sizeof(int)) > 0){
+			printf("%d ", buff);
+		}
+		return 0;
+	}
+	return 0;
 }
 
