2374ad800a5b20191dab36c6e4ad79bc4678419b
Mikayel <tach5674@gmail.com>
2022-11-06 18:40:31 +0400

    only Task1

--- hw-2022-11-07/Task2/main.c
+++ hw-2022-11-07/Task2/main.c
@@ -10,8 +10,31 @@ int main(int argc, char* argv[]){
 	char filename[100];
 	int i = 0;
 	int j = 0;
+	int t = 0;
 	int fd = open(argv[1],  O_RDWR);
-	
+	/*
+	args[0] = "wc";
+	args[1] = "-w";
+	args[2] = NULL;
+	pr[0] = 'w';
+	pr[1] = 'c';
+	filename[0] = 't';
+	filename[1] = 'a';
+	filename[2] = 's';
+	filename[3] = 'k';
+	*/
+	//Option 1
+	/*
+	dup2(fd, 0);
+	scanf("%s", pr);
+	scanf("%s", filename);
+	while(scanf("%s", args[j]) > 0){
+		++j;
+	}
+	args[j] = NULL;
+	*/
+	//Option 2
+	/*
 	while(read(fd, &buff, sizeof(buff)) > 0){
 		if(j == 0){
 			if(buff == ' '){
@@ -25,7 +48,7 @@ int main(int argc, char* argv[]){
 		if(j == 1){
 			if(buff == ' '){
 				++j;
-				i = 0; 
+				i = 0;
 			}
 			filename[i] = buff;
 			++i;
@@ -33,15 +56,16 @@ int main(int argc, char* argv[]){
 
 		}
 		if(buff == ' '){
-			args[j] = arg; 
-			++j;
+			args[t] = arg;
+			++t;
 			i = 0;
 		}
 		arg[i] = buff;
 		++i;
-	}
-	args[j+1] = NULL;
-	close(fd);
+	}*/
+	//args[t+1] = NULL;
+	
+	//close(fd);
 	int fd1 = open(filename, O_RDWR);
 	dup2(fd1, 0);
 	execvp(pr, args);
