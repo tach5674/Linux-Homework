15cf6fcd06cc05f28d4f9dbbdf3a61cba2b38842
tach5674 <63715658+tach5674@users.noreply.github.com>
2022-11-28 11:10:08 +0400

    task2 fixes

--- hw-2022-11-28/Task2/task2.c
+++ hw-2022-11-28/Task2/task2.c
@@ -3,30 +3,33 @@
 #include <unistd.h>
 
 int n = 0;
+int bytes = 0;
 
 void sigpipe_handler(int sig) {
-  printf("Broken pipe!\n");
+  printf("\nBroken pipe!\n%d\n", bytes);
   n = 1;
 }
 
-int main(void){
+int main(void) {
   signal(SIGPIPE, sigpipe_handler);
 
   int fd[2];
   pipe(fd);
 
-  if(fork() == 0){
+  if (fork() == 0) {
+    close(fd[1]);
     char str[5];
     read(fd[0], str, sizeof(str));
     write(1, str, sizeof(str));
     close(fd[0]);
-    close(fd[1]);
     _exit(0);
   }
 
+  close(fd[0]);
   char str[] = "Hello World";
-  while(n == 0){
-    write(fd[1], str, sizeof(str));
+  while (n == 0) {
+    bytes += write(fd[1], str, sizeof(str));
   }
+  close(fd[1]);
   _exit(1);
 }
